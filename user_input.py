75d7b276d0fdd39382bd8b23612466ef869225f9
ThomasKhamisi <146515659+ThomasKhamisi@users.noreply.github.com>
2024-02-26 13:57:47 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,13 @@
+# user_input.py
+
+# Ask the user for their name
+name = input("Thomas: ")
+
+# Ask the user for their age
+age = input("23: ")
+
+# Ask the user for their location
+location = input("Meru: ")
+
+# Print out a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
